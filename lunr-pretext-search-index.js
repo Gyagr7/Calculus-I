d2ec0e4c531007452f9ea3415b9ac192a656398b
Gyagr7 <136715739+Gyagr7@users.noreply.github.com>
2026-08-23 17:48:43 -0500

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,185 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "preface",
+  "level": "1",
+  "url": "preface.html",
+  "type": "Preface",
+  "number": "",
+  "title": "Preface",
+  "body": " Preface  [TODO: a short note to students -- what this course covers, how these notes are organized (each section moves from a motivating question, to a worked problem, to the general rule or theorem), and how the notes connect to the skills you'll be assessed on this semester.]  "
+},
+{
+  "id": "sec-derivatives-refresher",
+  "level": "1",
+  "url": "sec-derivatives-refresher.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "Derivative Rules and Critical Points",
+  "body": " Derivative Rules and Critical Points   [Motivation -- TODO: the question or problem that makes this worth learning]  [Problem -- TODO: a guiding worked example]  [Abstraction -- TODO: the general rule, definition, or theorem this section establishes]  "
+},
+{
+  "id": "sec-4-7-optimization",
+  "level": "1",
+  "url": "sec-4-7-optimization.html",
+  "type": "Section",
+  "number": "2.1",
+  "title": "Optimization Problems",
+  "body": " Optimization Problems   [Motivation -- TODO: the question or problem that makes this worth learning]  [Problem -- TODO: a guiding worked example]  [Abstraction -- TODO: the general rule, definition, or theorem this section establishes]  "
+},
+{
+  "id": "sec-4-8-newtons-method",
+  "level": "1",
+  "url": "sec-4-8-newtons-method.html",
+  "type": "Section",
+  "number": "2.2",
+  "title": "Newton’s Method",
+  "body": " Newton's Method   [Motivation -- TODO: the question or problem that makes this worth learning]  [Problem -- TODO: a guiding worked example]  [Abstraction -- TODO: the general rule, definition, or theorem this section establishes]  "
+},
+{
+  "id": "sec-4-9-antiderivatives",
+  "level": "1",
+  "url": "sec-4-9-antiderivatives.html",
+  "type": "Section",
+  "number": "2.3",
+  "title": "Antiderivatives",
+  "body": " Antiderivatives   [Motivation -- TODO: the question or problem that makes this worth learning]  [Problem -- TODO: a guiding worked example]  [Abstraction -- TODO: the general rule, definition, or theorem this section establishes]  "
+},
+{
+  "id": "sec-5-1-areas-distances",
+  "level": "1",
+  "url": "sec-5-1-areas-distances.html",
+  "type": "Section",
+  "number": "3.1",
+  "title": "Areas and Distances",
+  "body": " Areas and Distances   [Motivation -- TODO: the question or problem that makes this worth learning]  [Problem -- TODO: a guiding worked example]  [Abstraction -- TODO: the general rule, definition, or theorem this section establishes]  "
+},
+{
+  "id": "sec-5-2-definite-integral",
+  "level": "1",
+  "url": "sec-5-2-definite-integral.html",
+  "type": "Section",
+  "number": "3.2",
+  "title": "The Definite Integral",
+  "body": " The Definite Integral   [Motivation -- TODO: the question or problem that makes this worth learning]  [Problem -- TODO: a guiding worked example]  [Abstraction -- TODO: the general rule, definition, or theorem this section establishes]  "
+},
+{
+  "id": "sec-5-3-ftc",
+  "level": "1",
+  "url": "sec-5-3-ftc.html",
+  "type": "Section",
+  "number": "3.3",
+  "title": "The Fundamental Theorem of Calculus",
+  "body": " The Fundamental Theorem of Calculus   [Motivation -- TODO: the question or problem that makes this worth learning]  [Problem -- TODO: a guiding worked example]  [Abstraction -- TODO: the general rule, definition, or theorem this section establishes]  "
+},
+{
+  "id": "sec-5-4-indefinite-net-change",
+  "level": "1",
+  "url": "sec-5-4-indefinite-net-change.html",
+  "type": "Section",
+  "number": "3.4",
+  "title": "Indefinite Integrals and the Net Change Theorem",
+  "body": " Indefinite Integrals and the Net Change Theorem   [Motivation -- TODO: the question or problem that makes this worth learning]  [Problem -- TODO: a guiding worked example]  [Abstraction -- TODO: the general rule, definition, or theorem this section establishes]  "
+},
+{
+  "id": "sec-5-5-substitution",
+  "level": "1",
+  "url": "sec-5-5-substitution.html",
+  "type": "Section",
+  "number": "3.5",
+  "title": "The Substitution Rule",
+  "body": " The Substitution Rule   [Motivation -- TODO: the question or problem that makes this worth learning]  [Problem -- TODO: a guiding worked example]  [Abstraction -- TODO: the general rule, definition, or theorem this section establishes]  "
+},
+{
+  "id": "sec-6-1-areas-between-curves",
+  "level": "1",
+  "url": "sec-6-1-areas-between-curves.html",
+  "type": "Section",
+  "number": "4.1",
+  "title": "Areas Between Curves",
+  "body": " Areas Between Curves   [Motivation -- TODO: the question or problem that makes this worth learning]  [Problem -- TODO: a guiding worked example]  [Abstraction -- TODO: the general rule, definition, or theorem this section establishes]  "
+},
+{
+  "id": "sec-6-2-volumes",
+  "level": "1",
+  "url": "sec-6-2-volumes.html",
+  "type": "Section",
+  "number": "4.2",
+  "title": "Volumes (Disks and Washers)",
+  "body": " Volumes (Disks and Washers)   [Motivation -- TODO: the question or problem that makes this worth learning]  [Problem -- TODO: a guiding worked example]  [Abstraction -- TODO: the general rule, definition, or theorem this section establishes]  "
+},
+{
+  "id": "sec-6-3-cylindrical-shells",
+  "level": "1",
+  "url": "sec-6-3-cylindrical-shells.html",
+  "type": "Section",
+  "number": "4.3",
+  "title": "Volumes by Cylindrical Shells",
+  "body": " Volumes by Cylindrical Shells   [Motivation -- TODO: the question or problem that makes this worth learning]  [Problem -- TODO: a guiding worked example]  [Abstraction -- TODO: the general rule, definition, or theorem this section establishes]  "
+},
+{
+  "id": "sec-6-4-work",
+  "level": "1",
+  "url": "sec-6-4-work.html",
+  "type": "Section",
+  "number": "4.4",
+  "title": "Work",
+  "body": " Work   [Motivation -- TODO: the question or problem that makes this worth learning]  [Problem -- TODO: a guiding worked example]  [Abstraction -- TODO: the general rule, definition, or theorem this section establishes]  "
+},
+{
+  "id": "sec-6-5-average-value",
+  "level": "1",
+  "url": "sec-6-5-average-value.html",
+  "type": "Section",
+  "number": "4.5",
+  "title": "Average Value of a Function",
+  "body": " Average Value of a Function   [Motivation -- TODO: the question or problem that makes this worth learning]  [Problem -- TODO: a guiding worked example]  [Abstraction -- TODO: the general rule, definition, or theorem this section establishes]  "
+},
+{
+  "id": "sec-8-1-arc-length",
+  "level": "1",
+  "url": "sec-8-1-arc-length.html",
+  "type": "Section",
+  "number": "5.1",
+  "title": "Arc Length",
+  "body": " Arc Length   [Motivation -- TODO: the question or problem that makes this worth learning]  [Problem -- TODO: a guiding worked example]  [Abstraction -- TODO: the general rule, definition, or theorem this section establishes]  "
+},
+{
+  "id": "sec-8-2-surface-area",
+  "level": "1",
+  "url": "sec-8-2-surface-area.html",
+  "type": "Section",
+  "number": "5.2",
+  "title": "Area of a Surface of Revolution",
+  "body": " Area of a Surface of Revolution   [Motivation -- TODO: the question or problem that makes this worth learning]  [Problem -- TODO: a guiding worked example]  [Abstraction -- TODO: the general rule, definition, or theorem this section establishes]  "
+},
+{
+  "id": "sec-8-3-applications-physics-engineering",
+  "level": "1",
+  "url": "sec-8-3-applications-physics-engineering.html",
+  "type": "Section",
+  "number": "5.3",
+  "title": "Applications to Physics and Engineering",
+  "body": " Applications to Physics and Engineering   [Motivation -- TODO: the question or problem that makes this worth learning]  [Problem -- TODO: a guiding worked example]  [Abstraction -- TODO: the general rule, definition, or theorem this section establishes]  "
+},
+{
+  "id": "backmatter-2",
+  "level": "1",
+  "url": "backmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " These notes were authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
